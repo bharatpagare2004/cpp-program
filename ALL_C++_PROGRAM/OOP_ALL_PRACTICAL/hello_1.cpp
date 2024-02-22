616fd19a7ee408f55a33334455f0723989b92880
bharatpagare2004 <bharatpagare018@gmail.com>
2024-02-23 00:05:31 +0530

    add

---
+++ ALL_C++_PROGRAM/OOP_ALL_PRACTICAL/hello_1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>  
+using namespace std;  
+  
+// function to add two numbers  
+int add(int a, int b) {  
+    return a + b;  
+}  
+int main() {  
+    int num1, num2, sum;  
+    cout << "Enter two numbers: ";  
+    cin >> num1 >> num2;  
+  
+    // calling the add function and storing the result in sum  
+    sum = add(num1, num2);  
+    cout << "Sum of " << num1 << " and " << num2 << " is: " << sum << endl;  
+    return 0;  
+}  
+
---
+++ ALL_C++_PROGRAM/OOP_ALL_PRACTICAL/int.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a ,b,itesh;
+    cout<<"enter the value of no a:";
+    cin>>a;
+    cout<<"enter the value of no b:";
+    cin>>b;
+    itesh = a+b;
+    cout<<"the addition of a and b is :"<<itesh;
+    return 0;
+    
+}
